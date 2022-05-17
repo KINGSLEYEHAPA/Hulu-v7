ac8b7d6be8af56059f2996c853afc61103ee7d13
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-17 12:30:13 +0100

    Update of Navigation build

--- components/Nav.js
+++ components/Nav.js
@@ -2,12 +2,18 @@ import request from "../utilities/request";
 
 const Nav = () => {
   return (
-    <nav>
-      <div>
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(request).map(([key, { title, url }]) => (
-          <h2 key={title}>{title}</h2>
+          <h2
+            key={title}
+            className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
         ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202a] h-10 w-1/12" />
     </nav>
   );
 };
