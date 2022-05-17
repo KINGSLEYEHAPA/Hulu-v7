ffb376d09e9474e4f47d4c754fe558bfbe7f676c
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-17 12:12:51 +0100

    Update of Navigation build

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,13 @@
+import request from "../utilities/request";
+
 const Nav = () => {
   return (
     <nav>
-      <div></div>
+      <div>
+        {Object.entries(request).map(([key, { title, url }]) => (
+          <h2 key={title}>{title}</h2>
+        ))}
+      </div>
     </nav>
   );
 };
