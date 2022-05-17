276919eb588c9908dfb7cbc41637041dd4a32d2a
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-17 05:04:17 +0100

    Done with Header

--- components/Header.js
+++ components/Header.js
@@ -10,8 +10,8 @@ import {
 } from "@heroicons/react/outline";
 const Header = () => {
   return (
-    <header className="">
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto ">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItems title="HOME" Icon={HomeIcon} />
         <HeaderItems title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItems title="VERIFIED" Icon={BadgeCheckIcon} />
