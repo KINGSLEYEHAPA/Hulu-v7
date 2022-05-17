0a71ed94b1e752d08f89205824270ee52502bd89
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-17 04:56:05 +0100

    Building the header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import HeaderItems from "./HeaderItems";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  LightningBoltIcon,
+  CollectionIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 const Header = () => {
   return (
     <header className="">
-      <h1>The header</h1>
+      <div>
+        <HeaderItems title="HOME" Icon={HomeIcon} />
+        <HeaderItems title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItems title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItems title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItems title="SEARCH" Icon={SearchIcon} />
+        <HeaderItems title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt=""
+      />
     </header>
   );
 };
